7accee32141d3d574d2119fee4d70eafb5911bc7
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-04-21 18:04:29 +0530

    Some Changes Made

--- Authenticate/models.py
+++ Authenticate/models.py
@@ -10,8 +10,8 @@ class User(AbstractUser):
         ('vendor', 'Vendor'),
         ('customer', 'Customer'),
         ]
-    role = models.CharField(max_length=20 , choices= ROLE_CHOICE , default='customer')
     email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20 , choices= ROLE_CHOICE , default='customer')
 
     REQUIRED_FIELDS = ['email', 'role']
     def __str__(self):
