85585cbed6c8a3a4498b8191a970c6c069e94f14
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-04-22 11:49:01 +0530

    Updated User model

--- Authenticate/models.py
+++ Authenticate/models.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=20 , choices= ROLE_CHOICE , default='customer')
 
-    REQUIRED_FIELDS = ['email', 'role']
+    REQUIRED_FIELDS = ['email']
     def __str__(self):
         return self.username
 
