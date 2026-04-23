c967e71c11adf2dc21284f71fd83d3fb3351fa97
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-04-23 15:09:04 +0530

    Address Model Added

--- Authenticate/models.py
+++ Authenticate/models.py
@@ -17,3 +17,16 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+class Address(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE )
+    street = models.TextField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    pincode = models.CharField(max_length=6)
+    phone = models.CharField(max_length=15)
+    label = models.CharField(max_length=20 , choices=[('office' , 'Office'), ('house' , 'House')])
+
+    def __str__(self):
+        return f"{self.user.username} - {self.city} ({self.label})"
+    
+
