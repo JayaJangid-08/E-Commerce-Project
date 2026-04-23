1eadebf192363d55aa4a9aac9baaeb40f69fa5de
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-04-23 15:44:54 +0530

    Address Model Added

--- Authenticate/urls.py
+++ Authenticate/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
 urlpatterns = [
     path('register/', views.registration, name='register'),
     path('login/', views.login, name='login'),
+    path('refresh/token/', TokenRefreshView.as_view(), name='refresh-token'),
+    path('addresses/', views.address_list, name='adderss-list'),
+    # path('addresses/<int: address_id>/', views.address_detail, name='adderss-detail'),
 ]
