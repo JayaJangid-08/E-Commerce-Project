933fbfedab26fdcc8165bce1b73d006a76b91a1a
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-04-26 20:31:58 +0530

    Added Id

--- Authenticate/admin.py
+++ Authenticate/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import User
 
 # Register your models here.
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id','username','email','role']
+
+admin.site.register(User , UserAdmin)
