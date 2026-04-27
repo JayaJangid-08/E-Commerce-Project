fa11ef616f978623cd59d45c661667b2ed3bb107
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-04-27 13:00:46 +0530

    Updated Setting

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -116,7 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {  
     'DEFAULT_AUTHENTICATION_CLASSES': (  
         'rest_framework_simplejwt.authentication.JWTAuthentication',  
-    ),  
+    ),
+    'DEFAULT_PAGINATION_CLASS' :'rest_framework.pagination.PageNumberPagination',
 }
 
 
