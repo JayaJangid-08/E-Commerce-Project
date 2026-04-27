7cc3d566e198ef3f0af282441caca5020e06fed0
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-04-27 13:06:33 +0530

    URLs Updated

--- Products/urls.py
+++ Products/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     # Product URLs
-    path('products/', views.product_list, name='product_list'),
-    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('', views.product_list, name='product_list'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
 
     # Category URLs
     path('categories/', views.category_list, name='category_list'),
