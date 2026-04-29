5ab1703cb1eb7652a161b1f926ec6ccb0c63d093
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-04-29 13:20:14 +0530

    Added a new URL

--- Order/urls.py
+++ Order/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('place/', views.place_order, name='place-order'),
     path('<int:order_id>/', views.order_detail, name='order-detail'),
     path('<int:order_id>/cancel/',views.cancel_order, name='cancel-order'),
-    path('<int:order_id>/status/',views.update_status, name='update-status'),
+    path('<int:order_id>/status/',views.update_order_status, name='update-order-status'),
+    path('item/<int:item_id>/status/',views.update_item_status, name='update-item-status'),
 ]
