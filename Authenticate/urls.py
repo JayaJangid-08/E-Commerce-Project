a348139ae5e96cd0101339bd46a405265718f3f7
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-04-30 14:46:41 +0530

    TYPO Corrected

--- Authenticate/urls.py
+++ Authenticate/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('register/', views.registration, name='register'),
     path('login/', views.login, name='login'),
     path('refresh/token/', TokenRefreshView.as_view(), name='refresh-token'),
-    path('addresses/', views.address_list, name='adderss-list'),
-    path('addresses/<int:address_id>/', views.address_detail, name='adderss-detail'),
+    path('addresses/', views.address_list, name='address-list'),
+    path('addresses/<int:address_id>/', views.address_detail, name='address-detail'),
 ]
