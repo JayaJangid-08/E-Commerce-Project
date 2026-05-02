8f71c4694b1f07e9bf3cf2741ac705ca7a772e0f
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-05-02 11:23:36 +0530

    Make vendor and created_at fields read-only

--- Products/serializers.py
+++ Products/serializers.py
@@ -2,7 +2,8 @@ from .models import Product , Category
 from rest_framework import serializers
 
 class ProductSerializer(serializers.ModelSerializer):
-    created_at = serializers.DateTimeField(format="%Y-%m-%d")
+    created_at = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
+    vendor = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = Product
         fields = '__all__'
