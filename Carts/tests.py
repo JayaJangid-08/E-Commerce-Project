1bbceb15c94b772e17d89cf0020ca360569ccb4a
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-05-02 12:08:39 +0530

    Added Test For Cart List

--- Carts/tests.py
+++ Carts/tests.py
@@ -1,3 +1,92 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+from Authenticate.models import User
+from Products.models import Product, Category
+from .models import Cart
 
 # Create your tests here.
+
+class CartListTest(APITestCase):
+    def setUp(self):
+        self.vendor = User.objects.create_user(
+            username = 'NewVendor',
+            email = 'newvendor@gmail.com',
+            password = 'VendorPass',
+            role = 'vendor'
+        )
+        self.customer = User.objects.create_user(
+            username = 'NewCustomer',
+            email = 'newcustomer@gmail.com',
+            password = 'CustomerPass',
+            role = 'customer'
+        )
+        self.category = Category.objects.create(
+            name = 'Electronic'
+        )
+        self.product_1 = Product.objects.create(
+            name = 'NewProduct_1',
+            description = 'New Product is cool',
+            price = 120,
+            stock = 10,
+            category = self.category,
+            vendor = self.vendor,
+        )
+        self.product_2 = Product.objects.create(
+            name = 'NewProduct_2',
+            description = 'New Product is cool',
+            price = 120,
+            stock = 10,
+            category = self.category,
+            vendor = self.vendor,
+        )
+        self.cart = Cart.objects.create(
+            user = self.customer,
+            product = self.product_1,
+            quantity = 1
+        )
+        self.client.force_authenticate(user=self.customer)
+    
+    def test_get_cart_list_authenticate(self):
+        self.client.force_authenticate(user=self.customer)
+        response = self.client.get('/carts/cart/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_cart_list_unauthenticate(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get('/carts/cart/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    
+    def test_add_product_to_cart_as_customer(self):
+        data = {
+            'user' : self.customer.id,
+            'product' : self.product_2.id,
+            'quantity' : 1
+        }
+        self.client.force_authenticate(user=self.customer)
+        response = self.client.post('/carts/cart/add/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_add_product_to_cart_as_vendor(self):
+        data = {
+            'user' : self.customer.id,
+            'product' : self.product_2.id,
+            'quantity' : 1
+        }
+        self.client.force_authenticate(user=self.vendor)
+        response = self.client.post('/carts/cart/add/', data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_add_duplicate_product_to_cart_as_customer(self):
+        data = {
+            'user' : self.customer.id,
+            'product' : self.product_1.id,
+            'quantity' : 1
+        }
+        self.client.force_authenticate(user=self.customer)
+        response = self.client.post('/carts/cart/add/', data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+
+
