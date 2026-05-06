0e19fedaf90d81b9256f55309ae802e89c557179
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-05-06 13:01:59 +0530

    Added Role model

--- Authenticate/models.py
+++ Authenticate/models.py
@@ -3,22 +3,29 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class User(AbstractUser):
-    USERNAME_FIELD = 'username'
-    ROLE_CHOICE = [
+ROLE_CHOICE = [
         ('admin', 'Admin'),
         ('vendor', 'Vendor'),
         ('customer', 'Customer'),
         ]
+
+class Role(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'username'
     email = models.EmailField(unique=True)
-    role = models.CharField(max_length=20 , choices= ROLE_CHOICE , default='customer')
+    roles = models.ManyToManyField(Role)
 
     REQUIRED_FIELDS = ['email']
     def __str__(self):
         return self.username
 
 class Address(models.Model):
-    user = models.ForeignKey(User , on_delete=models.CASCADE )
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
     street = models.TextField()
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
