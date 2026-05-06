7fbfb0a17582123490bfa0756e2fe963e5ff3d37
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-05-06 13:01:07 +0530

    Display User Roles

--- Authenticate/admin.py
+++ Authenticate/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import User
+from .models import User , Address
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['id','username','email','role']
+    list_display = ['id','username','email','get_roles']
+    def get_roles(self, obj):
+        roles = obj.roles.values_list('name', flat=True)
+        return ", ".join(roles) if roles else "-"
+
+    get_roles.short_description = "Roles"
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user','city','state']
 
 admin.site.register(User , UserAdmin)
+admin.site.register(Address , AddressAdmin)
