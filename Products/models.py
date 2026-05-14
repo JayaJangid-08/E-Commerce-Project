629700ab64e7a717f6283c3c85d99e07472a95ba
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-05-14 10:05:26 +0530

    Image Of Product Added

--- Products/models.py
+++ Products/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.URLField(null=True, blank=True)
     stock = models.IntegerField()
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     vendor = models.ForeignKey(User, on_delete=models.CASCADE)
