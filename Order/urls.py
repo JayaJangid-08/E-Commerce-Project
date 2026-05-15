f0eb59cab7fbad8c3cce0f51629ad83f70e304e1
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-05-15 10:16:08 +0530

    URL Changed

--- Order/urls.py
+++ Order/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.order_list, name='order-list'),
     path('place/', views.place_order, name='place-order'),
     path('<int:order_id>/', views.order_detail, name='order-detail'),
-    path('<int:order_id>/cancel/',views.cancel_order, name='cancel-order'),
+    path('cancel-order-item/<int:item_id>/',views.cancel_order_item, name='cancel-order'),
     path('<int:order_id>/status/',views.update_order_status, name='update-order-status'),
     path('item/<int:item_id>/status/',views.update_item_status, name='update-item-status'),
 ]
