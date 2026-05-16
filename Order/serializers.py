e349bc127abc388df0b37e4f331e2c9245a633d5
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-05-16 13:21:19 +0530

    New Serializer Class Added

--- Order/serializers.py
+++ Order/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
-from . models import Order , OrderItem
+from . models import Order , OrderItem , OrderAddress
+
+class OrderAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderAddress
+        fields = [ 'id', 'full_name', 'street', 'city', 'state', 'pincode', 'latitude', 'longitude', 'phone']
+
 
 class OrderSerializer(serializers.ModelSerializer):
+    delivery_address = OrderAddressSerializer(read_only=True)
     class Meta:
         model = Order
         fields = ['id', 'customer', 'delivery_address', 'total_price', 'status', 'order_date']
-        read_only_fields = ['customer', 'total_price', 'order_date']
+        read_only_fields = ['customer', 'delivery_address', 'total_price', 'order_date']
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
@@ -13,4 +20,5 @@ class OrderItemSerializer(serializers.ModelSerializer):
         model = OrderItem
         fields = ['id', 'product', 'quantity', 'price']
         read_only_fields = ['price']
+
 
