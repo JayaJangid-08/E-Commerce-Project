e51bd169a77979c72fd4cc6f392cb3c089f73062
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-05-20 10:11:27 +0530

    Snapshot For Order Added

--- Order/models.py
+++ Order/models.py
@@ -42,13 +42,23 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+
+    # SNAPSHOT FIELDS
+    product_name = models.CharField(max_length=255)
+    product_description = models.TextField()
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_image = models.URLField(blank=True, null=True)
+    product_category = models.CharField(max_length=100, blank=True, null=True)
+    vendor_name = models.CharField(max_length=100, blank=True, null=True)
+
     quantity = models.PositiveIntegerField(default=1)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
-
-
+    
     def __str__(self):
-        return f"{self.order} - {self.product.name}"
+        return f"{self.order.id} - {self.product.name}"
+
+
 
 
