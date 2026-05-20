f5b752336e29f1c5d7cee783b51aa67920ef3d88
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-05-20 10:07:48 +0530

    Updation Made

--- Authenticate/serializers.py
+++ Authenticate/serializers.py
@@ -43,7 +43,7 @@ class AddressSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     class Meta : 
         model = Address
-        fields = ['id', 'street', 'city', 'state', 'pincode', 'phone', 'label', 'latitude', 'longitude']
+        fields = ['id', 'street', 'city', 'state', 'pincode', 'phone', 'latitude', 'longitude', 'label']
     
     def validate_pincode(self, value):
         if not value.isdigit() or len(value) != 6:
