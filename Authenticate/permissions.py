6c17dbd22d02220f420e18b4f4f21bfa136e8c4c
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-05-25 10:43:53 +0530

    New Permission Class Added

--- Authenticate/permissions.py
+++ Authenticate/permissions.py
@@ -1,21 +1,27 @@
 from .models import User
 from rest_framework import permissions
+from Warehouses.models import Warehouse
+
 
 class IsAuthenticatedBase(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated
 
+
 class IsAdmin(IsAuthenticatedBase):
     def has_permission(self, request, view):
         return super().has_permission(request, view) and request.user.roles.filter(name='admin').exists()
+
 
 class IsVendor(IsAuthenticatedBase):
     def has_permission(self, request, view):
         return super().has_permission(request, view) and request.user.roles.filter(name='vendor').exists()
 
+
 class IsCustomer(IsAuthenticatedBase):
     def has_permission(self, request, view):
         return super().has_permission(request, view) and request.user.roles.filter(name='customer').exists()
+
 
 class IsVendorOrAdmin(IsAuthenticatedBase):
     def has_permission(self, request, view):
@@ -25,7 +31,8 @@ class IsVendorOrAdmin(IsAuthenticatedBase):
         return super().has_permission(request, view) and (
             request.user.roles.filter(name='admin').exists() or
             obj.vendor == request.user)
-    
+
+
 class IsDiscountOwnerOrAdmin(IsAuthenticatedBase):
     def has_permission(self, request, view):
             return super().has_permission(request, view) and (request.user.roles.filter(name__in = ['admin', 'vendor']).exists())
@@ -35,9 +42,11 @@ class IsDiscountOwnerOrAdmin(IsAuthenticatedBase):
             request.user.roles.filter(name='admin').exists()
             or obj.creator == request.user
         )
+
+
 class IsReviewOwnerOrAdmin(IsAuthenticatedBase):
     def has_permission(self, request, view):
-            return super().has_permission(request, view) and (request.user.roles.filter(name__in = ['admin', 'vendor']).exists())
+            return super().has_permission(request, view) and (request.user.roles.filter(name__in = ['admin', 'customer']).exists())
 
     def has_object_permission(self, request, view, obj):
         return super().has_permission(request, view) and(
@@ -45,10 +54,31 @@ class IsReviewOwnerOrAdmin(IsAuthenticatedBase):
             or obj.user == request.user
         )
 
+
 class IsAdminOrOwner(IsAuthenticatedBase):
     def has_object_permission(self, request, view, obj):
-        return (
+        return super().has_permission(request, view) and (
             request.user.roles.filter(name='admin').exists() or
             obj.order.customer == request.user
         )
+
+
+class IsAdminOrAssignedStaff(IsAuthenticatedBase):
+    def has_permission(self, request, view):
+        return super().has_permission(request, view) and (request.user.roles.filter(name__in=['admin', 'staff']).exists())
+
+    def has_object_permission(self, request, view, obj):
+        # Admin can access everything
+        if request.user.roles.filter(name='admin').exists():
+            return True
+        
+        user_warehouses = Warehouse.objects.filter(warehouse_staff__staff=request.user)
+        # If object itself is Warehouse
+        if isinstance(obj, Warehouse):
+            return obj in user_warehouses
+        
+        # If object has warehouse field
+        if hasattr(obj, 'warehouse'):
+            return obj.warehouse in user_warehouses
+        return False
 
