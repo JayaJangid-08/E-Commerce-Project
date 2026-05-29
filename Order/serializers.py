b36819a1deda7d83ce208d562eb678d106705f7d
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-05-29 10:18:31 +0530

    Added New Feilds in 'fields' and 'read_only_fields'

--- Order/serializers.py
+++ Order/serializers.py
@@ -4,21 +4,31 @@ from . models import Order , OrderItem , OrderAddress
 class OrderAddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderAddress
-        fields = [ 'id', 'full_name', 'street', 'city', 'state', 'pincode', 'latitude', 'longitude', 'phone']
+        fields = [ 'id', 'full_name', 'street', 'city', 'state',
+                  'pincode', 'latitude', 'longitude', 'phone']
+
+    
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'product_name', 'product_description', 
+                'product_price', 'product_image', 'product_category', 
+                'vendor_name', 'quantity', 'status'
+            ]
+        read_only_fields = ['product_name', 'product_description', 'product_price',
+                            'product_image', 'product_category', 'vendor_name'
+                        ]
 
 
 class OrderSerializer(serializers.ModelSerializer):
     delivery_address = OrderAddressSerializer(read_only=True)
+    order_items = OrderItemSerializer(many=True, read_only=True)
     class Meta:
         model = Order
-        fields = ['id', 'customer', 'delivery_address', 'total_price', 'status', 'order_date']
-        read_only_fields = ['customer', 'delivery_address', 'total_price', 'order_date']
-
-
-class OrderItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrderItem
-        fields = ['id', 'product', 'product_name', 'product_description', 'product_price', 'product_image', 'quantity', 'status','price']
-        read_only_fields = ['price']
-
+        fields = ['id', 'customer', 'delivery_address', 'total_price', 'discount_amount',
+                  'final_price', 'status', 'order_date', 'order_items'
+                ]
+        read_only_fields = ['customer', 'delivery_address', 'discount_amount',
+                            'final_price', 'total_price', 'order_date'
+                        ]
 
