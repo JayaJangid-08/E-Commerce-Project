56d36d2a9e2e41267e04ad01395af49dd07f9957
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-05-29 10:12:59 +0530

    Removed Unwanted Status

--- Order/models.py
+++ Order/models.py
@@ -4,13 +4,19 @@ from Authenticate.models import User
 
 # Create your models here.
 
+class StatusChoice:
+    CONFIRMED = 'confirmed'
+    SHIPPED = 'shipped'
+    DELIVERED = 'delivered'
+    CANCELLED = 'cancelled'
+
+
 STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('confirmed', 'Confirmed'),
-        ('cancelled', 'Cancelled'),
-        ('shipped', 'Shipped'),
-        ('delivered', 'Delivered'),
-    ]
+        (StatusChoice.CONFIRMED, 'Confirmed'),
+        (StatusChoice.SHIPPED, 'Shipped'),
+        (StatusChoice.DELIVERED, 'Delivered'),
+        (StatusChoice.CANCELLED, 'Cancelled'),
+]
 
 
 class OrderAddress(models.Model):
@@ -31,10 +37,18 @@ class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     delivery_address = models.ForeignKey(OrderAddress, on_delete=models.CASCADE)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='confirmed')
     order_date = models.DateField(auto_now_add=True)
     discount_amount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     final_price = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+
+    def save(self, *args, **kwargs):
+        old_status = None
+        if self.pk:
+            old_status = Order.objects.get(pk=self.pk).status
+        super().save(*args, **kwargs)
+        if old_status != self.status:
+            self.order_items.update(status=self.status)
 
     def __str__(self):
         return f"{self.customer.username}"
@@ -54,10 +68,10 @@ class OrderItem(models.Model):
 
     quantity = models.PositiveIntegerField(default=1)
     
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='confirmed')
     
     def __str__(self):
-        return f"{self.order.id} - {self.product.name}"
+        return f"{self.order.id} - {self.product_name}"
 
 
 
