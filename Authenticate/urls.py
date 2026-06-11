3bcc0580a97ba7c43a948ca24168a489bdb4b91a
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-06-11 17:33:14 +0530

    Added Log-Out and Assign-Role

--- Authenticate/urls.py
+++ Authenticate/urls.py
@@ -5,8 +5,10 @@ from . import views
 urlpatterns = [
     path('register/', views.registration, name='register'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('refresh/token/', TokenRefreshView.as_view(), name='refresh-token'),
     path('addresses/', views.address_list, name='address-list'),
     path('address/<int:address_id>/', views.address_detail, name='address-detail'),
+    path('assign-role/', views.assign_role, name='assign-role'),
     path('switch-role/', views.switch_role, name='switch_role'),
 ]
