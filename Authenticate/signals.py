e08cc0ef8cc1a76d308eb0f8b3c6c32046edadef
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-06-19 15:37:26 +0530

    Updated Signal

--- Authenticate/signals.py
+++ Authenticate/signals.py
@@ -1,11 +1,14 @@
-# Authenticate/signals.py
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save 
 from django.dispatch import receiver
 from .models import User, Role
 
+# Authenticate/signals.py
+
 @receiver(post_save, sender=User)
 def assign_default_role(sender, instance, created, **kwargs):
-    if created:  # Applicabele on new user only
+    if created and not instance.is_superuser:  # Applicabele on new user only but not on SUPERUSER
         customer_role, _ = Role.objects.get_or_create(name='customer')
         instance.roles.add(customer_role)
+        User.objects.filter(pk=instance.pk).update(active_role='customer')
+
 
